12bfcbc662e9fc04ab8c779b24a729db1fc824d4
lourd <lourd@10.56.128.102>
2018-10-31 10:57:05 -0600

    Proyecto 2 trabajando

---
+++ estudiante.java
@@ -0,0 +1,15 @@
+
+package secy;
+
+/**
+ *
+ * @author lourd
+ */
+public class estudiante {
+    int carnet, dpi, credito, contraseña;
+    String nombre, correo, direccion, usuario;
+    estudiante semestre, estudiante_sig, estudiante_anterior;
+    public estudiante(){
+        
+    }
+}
