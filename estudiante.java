3400245e674e1bea69563dc8515725c0de9b513d
lourd <lourd@10.56.128.102>
2018-11-01 14:36:23 -0600

    Proyecto 2 trabajando

--- estudiante.java
+++ estudiante.java
@@ -9,7 +9,16 @@ public class estudiante {
     int carnet, dpi, credito, contraseña;
     String nombre, correo, direccion, usuario;
     estudiante semestre, estudiante_sig, estudiante_anterior;
-    public estudiante(){
-        
+    public estudiante( int carnet, int dpi, int credito, int contraseña, String nombre, String correo, String direccion, String usuario){
+        this.carnet = carnet;
+        this.dpi = dpi;
+        this.credito = credito;
+        this.contraseña = contraseña;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.usuario = usuario;
+        estudiante_sig = null;
+        estudiante_anterior = null;
     }
 }
