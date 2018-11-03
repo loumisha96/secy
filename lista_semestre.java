fac0dab6d3b313604b00eb5946cdc315c4c57775
lourd <lourd@10.56.128.102>
2018-11-02 23:28:42 -0600

    Proyecto 2 trabajando

---
+++ lista_semestre.java
@@ -0,0 +1,26 @@
+
+package secy;
+
+/**
+ *
+ * @author lourd
+ */
+public class lista_semestre {
+  int tamaño;
+  semestre primero;
+  public lista_semestre(){
+      tamaño = 0;
+      primero = null;
+  }
+  public boolean vacia(){
+      if( primero == null)
+          return true;
+      else
+          return false;
+  }
+  public void insertar(int año, Lista_curso curso){
+      if( vacia()){
+          
+      }
+  }
+}
