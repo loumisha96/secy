f9b57517f24eefb885c7f44a83418f0b0dd7c3a5
lourd <lourd@10.56.128.102>
2018-11-06 18:00:14 -0600

    Proyecto 2 trabajando

--- semestre.java
+++ semestre.java
@@ -8,10 +8,12 @@ package secy;
 public class semestre {
     int año;
     semestre siguiente;
+    semestre anterior;
     Lista_curso curso;
-    public semestre( int año){
+    public semestre( int año, Lista_curso curso){
         this.año = año;
         siguiente = null;
+        anterior = null;
         curso = null;
     }
     
