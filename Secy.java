6b736c469cbda93431157f155f11946c24c411b9
lourd <lourd@10.56.128.102>
2018-11-07 21:21:08 -0600

    Proyecto 2 trabajando

--- Secy.java
+++ Secy.java
@@ -13,21 +13,8 @@ public class Secy {
     public static void main(String[] args) {
         Lista_estudiante lista = new Lista_estudiante();
         lista_semestre semestre = new lista_semestre();
-        Curso curso = new Curso(5, 5, 77, 88, "carlos", "Si");
-        Lista_curso listacurso = new Lista_curso();
-        estudiante estudiante = new estudiante(1, 1, 1, 1, "nombre", "correo", "direccion"," usuario", semestre) ;
-        estudiante estudiante1 = new estudiante(2, 2, 2, 2, "nombre2", "correo2", "direccion2"," usuario2", semestre) ;
+       // Curso curso = new Curso(5, 5, 77, 88, "carlos", "Si");
         
-        
-        
-        lista.insertar(estudiante1);
-        //lista.insertar(estudiante1);
-        lista.insertar(estudiante);
-      
-        lista.mostrar();
-        estudiante1.carnet = 3;     
-        lista.modificar(estudiante);
-        lista.mostrar();
         
     }
 
