d763958ea0e97188bec9713ac63620214d97e5c7
lourd <lourd@10.56.128.102>
2018-11-08 11:14:00 -0600

    Proyecto 2 trabajando

--- modulo_estudiante.java
+++ modulo_estudiante.java
@@ -25,6 +25,11 @@ public class modulo_estudiante extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("ASIGNAR CURSOS");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("CURSOS APROBADOS");
 
@@ -62,6 +67,12 @@ public class modulo_estudiante extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        asignacion a = new asignacion();
+        a.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
