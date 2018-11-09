d99cbe7e754729a409be3adad11a552546cbaba1
lourd <lourd@10.56.128.102>
2018-11-09 00:39:53 -0600

    Proyecto 2 trabajando

--- semestre.java
+++ semestre.java
@@ -11,7 +11,7 @@ public class semestre {
     semestre anterior;
     Lista_curso curso;
     public semestre( int año, Lista_curso curso){
-        this.año = 2018;
+        this.año =2018 ; 
         this.curso = curso;
         siguiente = null;
         anterior = null;
